35750831b8218922ecaa95e03119cfbf5b20df80
Lyte Onyema <lyte.onyema@gmail.com>
2022-08-31 00:27:00 +0100

    make it better for heroku

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
@@ -64,7 +64,7 @@ return [
         ],
 
         'daily' => [
-            'driver' => 'errorlog',
+            'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
